fc19279bcd7b6c25d4264803c7596e5d9201b5ba
Josh The Geek <joshthegeek@me.com>
2013-10-29 21:40:15 -0400

    Clean up JOConstants and disable settings bundle, and don't copy podfile

--- Oracle/JOConstants.h
+++ Oracle/JOConstants.h
@@ -6,23 +6,40 @@
 //  Copyright (c) 2013 Joshua Oldenburg. All rights reserved.
 //
 
+// Modifiable
+#define JOUsePrefPane 0
+#define JOCauseErrorForTesting 0 // 0/1
+#define JOOracleFeedURL @"http://oraclenewspaper.com/feed/atom/"
+
+/* Defines:
+ * JOInfoSectionEnabled
+ * JOWebsiteLinkEnabled
+ * JODisableImageHeaderUniversally
+ * JOEnableImageHeaderOnIOS6
+ */
+
 extern NSString *const JOExceptionInvalid;
 extern NSString *const JOErrorDomain;
-
-#define JOInfoSectionEnabled [[NSUserDefaults standardUserDefaults] boolForKey:@"JOPrefShowInfoSection"] // YES
-#define JOWebsiteLinkEnabled [[NSUserDefaults standardUserDefaults] boolForKey:@"JOPrefShowWebsiteLink"] // YES
-
-#define JODisableImageHeaderUniversally (![[NSUserDefaults standardUserDefaults] boolForKey:@"JOPrefShowLogoInHeader"]) // NO // This overrides the following
-#define JOEnableImageHeaderOnIOS6 [[NSUserDefaults standardUserDefaults] boolForKey:@"JOPrefShowLogoInHeaderIn6"] // NO
-
-#define JOCauseErrorForTesting 0 // Please, preprocessor, make NO == 0 and YES == 1 in macros...
-
-#if JOCauseErrorForTesting
-#define JOOracleFeedURL @"http://oraclenewspaper.com/ThisPageShouldNotExist"
-#else
-#define JOOracleFeedURL @"http://oraclenewspaper.com/feed/atom/"
-#endif
 
 @interface JOUtil : NSObject
 
 @end
+
+// Implementation details
+#if JOUsePrefPane
+	#define JOInfoSectionEnabled [[NSUserDefaults standardUserDefaults] boolForKey:@"JOPrefShowInfoSection"] // YES
+	#define JOWebsiteLinkEnabled [[NSUserDefaults standardUserDefaults] boolForKey:@"JOPrefShowWebsiteLink"] // YES
+	#define JODisableImageHeaderUniversally (![[NSUserDefaults standardUserDefaults] boolForKey:@"JOPrefShowLogoInHeader"]) // NO // This overrides the following
+	#define JOEnableImageHeaderOnIOS6 [[NSUserDefaults standardUserDefaults] boolForKey:@"JOPrefShowLogoInHeaderIn6"] // NO
+	#warning Using debug pref pane
+#else
+	#define JOInfoSectionEnabled YES
+	#define JOWebsiteLinkEnabled YES
+	#define JODisableImageHeaderUniversally NO
+	#define JOEnableImageHeaderOnIOS6 NO
+#endif
+
+#if JOCauseErrorForTesting
+	#undef JOOracleFeedURL
+	#define JOOracleFeedURL @"http://oraclenewspaper.com/ThisPageShouldNotExist"
+#endif
